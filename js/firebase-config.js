b60979c06bb8baf9b9699ff3a05016e5b0794f5b
Namkha-yolo <122397692+Namkha-yolo@users.noreply.github.com>
2025-05-10 14:42:22 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjuFREFHDRe70efEwp-8RHRN0atLp6gjo",
   authDomain: "partime-389b2.firebaseapp.com",
   projectId: "partime-389b2",
-  storageBucket: "partime-389b2.appspot.com",
+  storageBucket: "partime-389b2.firebasestorage.app",
   messagingSenderId: "481270697427",
   appId: "1:481270697427:web:a4c357f69fd5d5f8257326",
   measurementId: "G-V1WY1KYZYL"
@@ -16,3 +16,15 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 console.log("Firebase initialized successfully!");
+
+// Add to the end of your firebase-config.js file
+db.collection('test').add({
+  message: 'Firebase connection test',
+  timestamp: new Date()
+})
+.then(() => {
+  console.log('Firebase connection successful!');
+})
+.catch((error) => {
+  console.error('Firebase connection failed:', error);
+});
