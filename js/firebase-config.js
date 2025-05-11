f9e370eda43569608bbac3ca025b8caa8ad055b7
Namkha-yolo <122397692+Namkha-yolo@users.noreply.github.com>
2025-05-11 17:15:22 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjuFREFHDRe70efEwp-8RHRN0atLp6gjo",
   authDomain: "partime-389b2.firebaseapp.com",
   projectId: "partime-389b2",
-  storageBucket: "partime-389b2.firebasestorage.app",
+  storageBucket: "partime-389b2.appspot.com",
   messagingSenderId: "481270697427",
   appId: "1:481270697427:web:a4c357f69fd5d5f8257326",
   measurementId: "G-V1WY1KYZYL"
