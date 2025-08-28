4d186466fb53ba3f207782ea15d00dfeed6bc1fb
Namkha-yolo <oedzernamkha@gmail.com>
2025-08-28 13:17:39 -0400

    should work now

--- api/index.js
+++ api/index.js
@@ -64,7 +64,9 @@ const connectToDatabase = async () => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.NODE_ENV === 'production' 
+    ? `https://${process.env.VERCEL_URL || 'www.only-works.com'}/auth/google/callback`
+    : "/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     await connectToDatabase();
